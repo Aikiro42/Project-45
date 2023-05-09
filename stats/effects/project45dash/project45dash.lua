73689fa5ba70895f9569d72a042c18eeea419f3b
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2023-05-10 00:38:22 +0800

    Updated dash vfx

--- stats/effects/project45dash/project45dash.lua
+++ stats/effects/project45dash/project45dash.lua
@@ -1,29 +1,36 @@
 function init()
   -- invulnerability
+  animator.setParticleEmitterOffsetRegion("explosion", mcontroller.boundBox())
   effect.addStatModifierGroup({{stat = "invulnerable", amount = 1}})
-
+  effect.addStatModifierGroup({{stat = "powerMultiplier", effectiveMultiplier = 0}})
+  self.maxDuration = effect.duration()
+  animator.setParticleEmitterBurstCount("explosion", 10)
+  animator.burstParticleEmitter("explosion")
+  animator.setParticleEmitterActive("explosion", true)
   -- turn BLUE and become transparent
-  effect.setParentDirectives("?fade=FFFFFF=0.1?multiply=FFFFFF3F")
-
+  -- self.direk = "?fade=FFFFFF=1?multiply=000000FA?outline=1;BE00FFCC;00000000"
+  self.direk = "?fade=FFFFFF=0.1?multiply=54007199"
+  effect.setParentDirectives(self.direk)
+  
 end
 
 function update(dt)
 
   -- turn BLUE and become transparent
-  effect.setParentDirectives("?fade=FFFFFF=0.1?multiply=FFFFFF3F")
+  effect.setParentDirectives(self.direk)
+  
+  local speedVector = mcontroller.velocity()
+  local speedThreshold = {0.1, 0.5}
 
   -- particles
-  if mcontroller.xVelocity() ~= 0 or mcontroller.yVelocity() ~= 0 then
+  if math.abs(speedVector[1]) > 0.75 or math.abs(speedVector[2]) > 1.3 then
     if mcontroller.facingDirection() == 1 then
-      animator.setParticleEmitterActive("dodgeRight", true)
-      animator.setParticleEmitterActive("dodgeLeft", false)
+      animator.burstParticleEmitter("dodgeRight", true)
+      -- animator.burstParticleEmitter("dodgeLeft", false)
     else
-      animator.setParticleEmitterActive("dodgeRight", false)
-      animator.setParticleEmitterActive("dodgeLeft", true)
+      -- animator.burstParticleEmitter("dodgeRight", false)
+      animator.burstParticleEmitter("dodgeLeft", true)
     end
-  else
-    animator.setParticleEmitterActive("dodgeRight", false)
-    animator.setParticleEmitterActive("dodgeLeft", false)
   end
 
 end
