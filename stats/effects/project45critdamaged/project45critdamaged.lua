ea357d4a117b65b8803ed6c8e6e3eb57b17c0e21
Aikiro42 <hiroku042@gmail.com>
2023-10-25 22:37:30 +0800

    Reimplement crit damage indicator
    - indicator status effect now uses a damage listener that expires the effect when the (crit) damage is received

--- stats/effects/project45critdamaged/project45critdamaged.lua
+++ stats/effects/project45critdamaged/project45critdamaged.lua
@@ -1,32 +1,66 @@
+require "/scripts/util.lua"
+require "/scripts/status.lua"
+
 function init()
-
-  self.critvfxtime = 0.1
-  
-  -- guarantee that this status effect always lasts 0.1s
-  if effect.duration() > self.critvfxtime then
-    effect.modifyDuration(-effect.duration() + self.critvfxtime)
-  end
-
-  animator.setParticleEmitterOffsetRegion("damagedParticle", mcontroller.boundBox())
-  animator.burstParticleEmitter("damagedParticle")
-  animator.burstParticleEmitter("criticalParticle")
-  animator.playSound("damagedSound")
-  self.critvfxtimer = self.critvfxtime
+  self.damageListener = damageListener("damageTaken", function(notifications)
+    if #notifications > 0 then
+      for _, notification in pairs(notifications) do
+        if notification.healthLost > 0 and world.entityExists(notification.targetEntityId) then
+          spawnCritVFX(notification.targetEntityId)
+          effect.expire()
+        end
+      end
+    end
+  end)
 end
 
 function update(dt)
-
-  self.critvfxtimer = math.max(0, self.critvfxtimer - dt)
-  if self.critvfxtimer == 0 then
-    animator.burstParticleEmitter("damagedParticle")
-    animator.burstParticleEmitter("criticalParticle")
-    animator.playSound("damagedSound")
-    self.critvfxtimer = self.critvfxtime 
-  end
-
-  animator.burstParticleEmitter("damagedParticle")
+  self.damageListener:update()
 end
 
 function uninit()
+end
 
+function spawnCritVFX(targetEntityId)
+  -- Code from Chofranc
+  critAction = {
+      timeToLive = 0,
+      damageType = "NoDamage",
+      actionOnReap = {
+        {
+          action = "particle",
+          specification = {
+            type = "text",
+            text =  "^shadow;CRIT!",
+            color = {255, 98, 0, 180},
+            light = {69, 26, 0},
+            destructionAction = "fade",
+            destructionTime =  0.5,
+            layer = "front",
+            position = {0,0},
+            size = 0.5,
+            approach = {40, 40},
+            initialVelocity = {0.0, 20.0},
+            finalVelocity = {0.0, 0.0},
+            angularVelocity = 0,
+            flippable = false,
+            timeToLive = 0,
+            fullbright = true,
+            rotation = 0,
+            variance = {
+              initialVelocity = {20, 5}
+            }
+          }
+        },
+        {
+          action = "sound",
+          options = {
+            "/sfx/project45neosfx/bulletimpact/bulletimpact-organic1.ogg",
+            "/sfx/project45neosfx/bulletimpact/bulletimpact-organic2.ogg",
+            "/sfx/project45neosfx/bulletimpact/bulletimpact-organic3.ogg"
+          }
+        }
+      }
+    }
+  world.spawnProjectile("invisibleprojectile", world.entityPosition(targetEntityId), targetEntityId, {0,0}, true,critAction)
 end
