e0fad1cb2af64a15420e5b8a0cdacfaf1aa4442d
Aikiro42 <hiroku042@gmail.com>
2024-04-24 13:37:31 +0800

    fix: change project45-aegisaltmod
    - Nerfed per piece damage from 10% to 5%
    - Rewrite passive description

--- items/augments/project45-gunmods/mods/project45-aegisaltmod/project45-aegisaltmod.lua
+++ items/augments/project45-gunmods/mods/project45-aegisaltmod/project45-aegisaltmod.lua
@@ -3,37 +3,33 @@ require "/scripts/set.lua"
 Passive = {}
 
 function Passive:init()
-  if player then
 
-    if not storage.acceleratorArmor then
-      storage.acceleratorArmor = set.new(self.passiveParameters.validArmor or {})
-      local races = {"human", "apex", "hylotl", "floran", "glitch", "novakid", "avian"}
-      for _, race in ipairs(races) do
-        storage.acceleratorArmor[race .. "tier5ahead"] = true
-        storage.acceleratorArmor[race .. "tier5achest"] = true
-        storage.acceleratorArmor[race .. "tier5alegs"] = true
-        storage.acceleratorArmor[race .. "tier6ahead"] = true
-        storage.acceleratorArmor[race .. "tier6achest"] = true
-        storage.acceleratorArmor[race .. "tier6alegs"] = true
-      end
+  if not storage.acceleratorArmor then
+    storage.acceleratorArmor = set.new(self.passiveParameters.validArmor or {})
+    local races = {"human", "apex", "hylotl", "floran", "glitch", "novakid", "avian"}
+    for _, race in ipairs(races) do
+      storage.acceleratorArmor[race .. "tier5ahead"] = true
+      storage.acceleratorArmor[race .. "tier5achest"] = true
+      storage.acceleratorArmor[race .. "tier5apants"] = true
+      storage.acceleratorArmor[race .. "tier6ahead"] = true
+      storage.acceleratorArmor[race .. "tier6achest"] = true
+      storage.acceleratorArmor[race .. "tier6apants"] = true
     end
-    
-    self.passiveDamageMult = 1
-    
-    local damageMultAdd = 0
-    
-    -- TODO: try using tags
-    damageMultAdd = damageMultAdd + (storage.acceleratorArmor[player.equippedItem("head").name] and 0.1 or 0)
-    damageMultAdd = damageMultAdd + (storage.acceleratorArmor[player.equippedItem("chest").name] and 0.1 or 0)
-    damageMultAdd = damageMultAdd + (storage.acceleratorArmor[player.equippedItem("legs").name] and 0.1 or 0)
-    self.passiveDamageMult = 1 + damageMultAdd
+  end
 
+  if player then
+    local damageMultAdd = 0
+
+    -- TODO: try using tags
+    local isHead = storage.acceleratorArmor[(player.equippedItem("head") or {}).name or "???"]
+    local isChest = storage.acceleratorArmor[(player.equippedItem("chest") or {}).name or "???"]
+    local isLegs = storage.acceleratorArmor[(player.equippedItem("legs") or {}).name or "???"]
+    damageMultAdd = damageMultAdd + (isHead and self.passiveParameters.pieceDamage or 0)
+    damageMultAdd = damageMultAdd + (isChest and self.passiveParameters.pieceDamage or 0)
+    damageMultAdd = damageMultAdd + (isLegs and self.passiveParameters.pieceDamage or 0)
+    self.passiveDamageMult = 1 + damageMultAdd
   end
 end
 
 function Passive:update(dt, fireMode, shiftheld)
-end
-
-function Passive:uninit()
-
 end
