70a0929c71b47650a20b6cf78307ec8a9e092970
Aikiro42 <hiroku042@gmail.com>
2024-04-25 15:56:59 +0800

    feat: add switch mode alt ability
     designed to work with project45gunfire

---
+++ items/active/weapons/ranged/abilities/project45gunfireswitch/project45gunfireswitch.lua
@@ -0,0 +1,66 @@
+require "/scripts/util.lua"
+require "/items/active/weapons/project45neoweapon.lua"
+-- require "/items/active/weapons/ranged/gunfire.lua"
+
+Project45GunFireSwitch = WeaponAbility:new()
+
+function Project45GunFireSwitch:init()
+
+  -- find altAbility
+  for i, ability in ipairs(self.weapon.abilities) do
+    if ability.name == "_Project45GunFire_" then
+      self.abilityIndex = i
+      break
+    end
+  end
+
+  -- initialize mode setting
+  self.modeIndex = 0
+  self.modes[0] = {}
+  
+end
+
+function Project45GunFireSwitch:update(dt, fireMode, shiftHeld)
+
+  WeaponAbility.update(self, dt, fireMode, shiftHeld)
+
+  if self.fireMode ~= (self.activatingFireMode or self.abilitySlot) then
+    self.triggered = false
+  end
+
+  if self.fireMode == "alt" and not self.triggered then
+    -- triggered; switch modes
+    animator.playSound("click")
+    self.modeIndex = (self.modeIndex + 1) % (#self.modes + 1)
+    self:switch(self.modes[self.modeIndex])
+    self.triggered = true
+  end
+
+end
+
+
+function Project45GunFireSwitch:switch(altConfig)
+  if not altConfig then
+    util.mergeTable(self.weapon.abilities[self.abilityIndex], self.modes[0])
+    return
+  end
+  
+  for param, _ in pairs(altConfig) do
+    if self.modes[0][param] == nil then
+      local val = self.weapon.abilities[self.abilityIndex][param]
+      sb.logInfo(string.format("%s: %s", param, sb.print(val, 1)))
+      if type(val) == "table" then
+        self.modes[0][param] = sb.jsonMerge({}, val)
+      else
+        self.modes[0][param] = val or false -- nil is a falsy value
+      end
+    end
+  end
+  util.mergeTable(self.weapon.abilities[self.abilityIndex], altConfig)
+  sb.logInfo(self.modeIndex)
+  sb.logInfo(sb.printJson(self.weapon.abilities[self.abilityIndex].manualFeed))
+  -- self.weapon.forceReset = true
+end
+
+function Project45GunFireSwitch:uninit()
+end
