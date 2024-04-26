b48409fe51c3810e4d52c771c8636e46342612ae
Aikiro42 <hiroku042@gmail.com>
2024-04-26 12:05:05 +0800

    feat: improve orbital strike
    - orbital strike projectiles now "collide" a limited number of times before dying and exploding.
    - orbital strike now has a height limit, defaulting to 1/8 from the bottom of a world.
    - Cooldown has been reduced from 10s to 3s
    - Energy cost increased from 200 to 300
    - Improved targeting VFX
    - orbital strike can now function in space. It just spawns explosions in the target area if the gravity in that area is below the gravThreshold
    - the default orbital strike projectiles now destroy blocks, fitting for bunker-busters, since they're supposed to bust bunks.

--- items/active/weapons/ranged/abilities/project45orbitalstrike/project45orbitalstrikeanimation.lua
+++ items/active/weapons/ranged/abilities/project45orbitalstrike/project45orbitalstrikeanimation.lua
@@ -18,6 +18,8 @@ function update()
 
   local strikeHeight = animationConfig.animationParameter("strikeHeight")
 
+  local gravThreshold = 0.1
+
   local drawStrikeLaser = animationConfig.animationParameter("drawStrikeLaser")
 
   local strikeCoordY = animationConfig.animationParameter("strikeCoordY")
@@ -25,12 +27,18 @@ function update()
   local laserTick = animationConfig.animationParameter("strikeLaserTick")
   local laserColor = animationConfig.animationParameter("strikeLaserColor") or {0, 200, 255, 225}
 
+  local gradientSteps = 64
+
   local laserStart, laserEnd
 
   if animationConfig.animationParameter("drawStrikeLaser") then
 
     if laserTick then
-      laserStart = vec2.add(activeItemAnimation.ownerAimPosition(), {0, strikeHeight})
+      laserStart = activeItemAnimation.ownerAimPosition()
+
+      if world.gravity(laserStart) > gravThreshold then
+        laserStart = vec2.add(laserStart, {0, strikeHeight})
+      end
       laserEnd = vec2.add(activeItemAnimation.ownerAimPosition(), {0, -strikeHeight})
       if strikeCoordY then
         laserEnd[2] = strikeCoordY
@@ -42,6 +50,15 @@ function update()
         fullbright = true,
         color = laserColor
       }, "Player-2")
+      drawGradient(laserEnd, vec2.add(laserEnd, {0, -5}), laserWidth, gradientSteps, laserColor, true, "Player-2")
+      
+      drawGradient(laserEnd, vec2.add(laserEnd, {0, 5}), laserWidth, gradientSteps, {255, 0, 0, 128}, true, "overlay")
+      
+      drawGradient(laserEnd, vec2.add(laserEnd, {-10, 0}), 0.5, 4, {255, 128, 0, 255}, true, "overlay")
+      drawGradient(laserEnd, vec2.add(laserEnd, {10, 0}), 0.5, 4, {255, 128, 0, 255}, true, "overlay")
+      
+      drawGradient(laserEnd, vec2.add(laserEnd, {0, -5}), laserWidth, gradientSteps, {255, 0, 0, 128}, true, "overlay")
+
     end
 
   end
@@ -59,26 +76,58 @@ function update()
       if strikeCoordY then
         orbitalLaserEnd[2] = strikeCoordY
       end
+      if world.gravity(orbitalLaserPos) > gravThreshold then
+        local orbitalLine = worldify(orbitalLaserStart, orbitalLaserEnd)
+        localAnimator.addDrawable({
+          line = orbitalLine,
+          width = orbitalLaserWidth,
+          fullbright = true,
+          color = orbitalColor
+        }, "overlay")
+        localAnimator.addDrawable({
+          line = orbitalLine,
+          width = orbitalLaserWidth / 4,
+          fullbright = true,
+          color = {255, 255, 255, orbitalColor[4]}
+        }, "overlay")
+      else
+        drawGradient(orbitalLaserEnd, vec2.add(orbitalLaserEnd, {0, 5}), orbitalLaserWidth, gradientSteps, orbitalColor, true, "overlay")
+        drawGradient(orbitalLaserEnd, vec2.add(orbitalLaserEnd, {0, 5}), orbitalLaserWidth / 4, gradientSteps, {255, 255, 255, orbitalColor[4]}, true, "overlay")  
+      end
 
-      local orbitalLine = worldify(orbitalLaserStart, orbitalLaserEnd)
+      drawGradient(orbitalLaserEnd, vec2.add(orbitalLaserEnd, {0, -5}), orbitalLaserWidth, gradientSteps, orbitalColor, true, "overlay")
+      drawGradient(orbitalLaserEnd, vec2.add(orbitalLaserEnd, {0, -5}), orbitalLaserWidth / 4, gradientSteps, {255, 255, 255, orbitalColor[4]}, true, "overlay")
 
-      localAnimator.addDrawable({
-        line = orbitalLine,
-        width = orbitalLaserWidth,
-        fullbright = true,
-        color = orbitalColor
-      }, "overlay")
-      localAnimator.addDrawable({
-        line = orbitalLine,
-        width = orbitalLaserWidth / 4,
-        fullbright = true,
-        color = {255, 255, 255, orbitalColor[4]}
-      }, "overlay")
+      orbitalColor[4] = math.floor(orbitalColor[4] / 2)
+
 
     end
 
   end
 
+end
+
+function drawGradient(startPos, endPos, width, steps, color, fullbright, layer)
+  layer = layer or "overlay"
+  if #color < 3 then
+    table.insert(color, 255)
+  end
+  local gradientLength = world.magnitude(startPos, endPos)
+  local stepLength = gradientLength/steps
+  local colorDec = color[4]/steps
+  local normalVector = vec2.norm(world.distance(endPos, startPos))
+  for i=0, steps do
+    local stepEnd = vec2.add(startPos, vec2.mul(normalVector, stepLength))
+    local gradientLine = worldify(startPos, stepEnd)
+    local newColor = {color[1], color[2], color[3], math.max(0, color[4] - colorDec * i)}
+    localAnimator.addDrawable({
+      line = gradientLine,
+      width = width,
+      fullbright = fullbright,
+      color = newColor
+    }, layer)
+    startPos = stepEnd
+  end
 end
 
 -- Calculates the line from pos1 to pos2
