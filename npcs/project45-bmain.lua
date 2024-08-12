fb445e44175b82a4fdc62cf281a6e9de7eab3375
Aikiro42 <hiroku042@gmail.com>
2024-08-13 01:05:56 +0800

    feat: improve project45-enemyflare mechanics
    - changed damage and energy usage parameters of npc weapons
    - added project45-neo-npc-shotgun
    - added project45goon.npctype
    - project45-enemyflare now spawns project 45 goons, armed with a black Seeker's Set and (lesser versions of some) project 45 armaments
    - added beam in effect to project45-enemyflare spawns
    - improved spawn-in algorithm of project45-enemyflare
    - added custom drop table for project 45 goons

---
+++ npcs/project45-bmain.lua
@@ -0,0 +1,88 @@
+require "/scripts/behavior.lua"
+require "/scripts/pathing.lua"
+require "/scripts/util.lua"
+require "/scripts/vec2.lua"
+require "/scripts/quest/participant.lua"
+require "/scripts/relationships.lua"
+require "/scripts/actions/dialog.lua"
+require "/scripts/actions/movement.lua"
+require "/scripts/drops.lua"
+require "/scripts/statusText.lua"
+require "/scripts/tenant.lua"
+require "/scripts/companions/recruitable.lua"
+require "/npcs/bmain.lua"
+
+-- Engine callback - called on initialization of entity
+function init()
+  restorePreservedStorage()
+
+  self.shouldDie = true
+  self.forceDie = false
+  self.pathing = {}
+
+  self.notifications = {}
+  if storage.spawnPosition == nil then
+    local position = mcontroller.position()
+    local groundPosition = findGroundPosition(position, -20, 3)
+    storage.spawnPosition = groundPosition or position
+  end
+
+  local questOutbox = Outbox.new("questOutbox", ContactList.new("questContacts"))
+  self.quest = QuestParticipant.new("quest", questOutbox)
+  self.quest.onOfferedQuestStarted = offeredQuestStarted
+  self.quest.onOfferedQuestFinished = offeredQuestFinished
+
+  if config.getParameter("behavior") then
+    self.behavior = behavior.behavior(config.getParameter("behavior"), config.getParameter("behaviorConfig", {}), _ENV)
+
+    self.board = self.behavior:blackboard()
+    self.board:setPosition("spawn", storage.spawnPosition)
+  end
+
+  npc.setInteractive(true)
+  script.setUpdateDelta(10)
+
+  self.behaviorConfig = config.getParameter("behaviorConfig", {})
+  if personality().behaviorConfig then
+    self.behaviorConfig = applyDefaults(personality().behaviorConfig, self.behaviorConfig)
+  end
+
+  self.primary = npc.getItemSlot("primary")
+  self.alt = npc.getItemSlot("alt")
+  self.sheathedPrimary = npc.getItemSlot("sheathedprimary")
+  self.sheathedAlt = npc.getItemSlot("sheathedalt")
+  self.delayedSetItemSlot = {}
+
+  self.debug = false
+
+  mcontroller.setAutoClearControls(false)
+
+  self.stuckCheckTime = config.getParameter("stuckCheckTime", 3.0)
+  self.stuckCheckTimer = 0.1
+
+  message.setHandler("notify", function (_, _, notification)
+      return notify(notification)
+    end)
+
+  message.setHandler("suicide", function ()
+      status.setResource("health", 0)
+    end)
+
+  self.uniqueId = config.getParameter("uniqueId")
+  updateUniqueId()
+
+  npc.setDamageOnTouch(true)
+  npc.setAggressive(config.getParameter("aggressive", false))
+
+  if (config.getParameter("facingDirection")) then
+    mcontroller.controlFace(config.getParameter("facingDirection"))
+  end
+
+  recruitable.init()
+
+  if type(extraInit) == "function" then
+    extraInit()
+  end
+  status.addEphemeralEffect("beamin")
+
+end
--- projectiles/throwable/project45/project45-enemyflare/project45-enemyflare.lua
+++ projectiles/throwable/project45/project45-enemyflare/project45-enemyflare.lua
@@ -42,13 +42,54 @@ function atEntityCap(entityCap)
   return #entities >= entityCap
 end
 
-function spawnEnemy()
-  local humanoidCollisionPoly = {{-0.75,-2},{-0.35,-2.5},{0.35,-2.5},{0.75,-2},{0.75,0.65},{0.35,1.22},{-0.35,1.22},{-0.75,0.65}}
-  local spawnPosition = vec2.add(mcontroller.position(), {sb.nrand(self.spawnRadius, 0), 0})
-  spawnPosition = world.resolvePolyCollision(humanoidCollisionPoly, spawnPosition, math.max(5, self.spawnRadius)) or spawnPosition
+function spawnEnemy(position, spawnPointCount)
+  determineSpawnPoints(position, spawnPointCount)
+  -- do not spawn if no valid spawn point
+  if #self.spawnPositions == 0 then return end
+  local spawnPosition = util.randomFromList(self.spawnPositions)
   local chosenType = self.npcTypes[math.ceil(math.random() * #self.npcTypes)]
   local chosenRace = self.npcRaces[math.ceil(math.random() * #self.npcRaces)]
-
   local spawned = world.spawnNpc(spawnPosition, chosenRace, chosenType, self.npcLevel, nil, self.npcOverrides)
+end
+
+function determineSpawnPoints(position, spawnPointCount)
+  
+  position = position or mcontroller.position()
+  if self.lastBaseSpawnPos then
+    if self.lastBaseSpawnPos[1] == position[1]
+    and self.lastBaseSpawnPos[2] == position[2]
+    then return end
+  end
+  self.lastBaseSpawnPos = position
+  
+  local humanoidCollisionPoly = {{-0.75,-2},{-0.35,-2.5},{0.35,-2.5},{0.75,-2},{0.75,0.65},{0.35,1.22},{-0.35,1.22},{-0.75,0.65}}
+  
+  spawnPointCount = spawnPointCount or 3
+  self.spawnPositions = {nil}
+  
+  for i=1, spawnPointCount do
+    -- take random point within horizontal radius
+    local point = vec2.add(mcontroller.position(), {sb.nrand(self.spawnRadius, 0), 0})
+
+    -- detect collision downwards
+    local ground = world.lineCollision(vec2.add(point, {0, 50}), point, {"Block", "Dynamic"})
+
+    -- attempt to resolve poly collision
+    local viable =
+      world.resolvePolyCollision(
+        humanoidCollisionPoly,
+        ground or point,
+        math.max(5, self.spawnRadius),
+        {"Block", "Dynamic"}
+      )
+      or ground
+      or point
+
+      -- do not add point if still collides
+    if not world.polyCollision(humanoidCollisionPoly, viable, {"Block", "Dynamic"}) then
+      table.insert(self.spawnPositions, viable)
+    end
+    
+  end
 
 end
