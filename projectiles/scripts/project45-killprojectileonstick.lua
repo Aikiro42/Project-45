0977d0fa64609b82e6e656a687cc7f67b8206b48
Aikiro42 <hiroku042@gmail.com>
2024-08-15 10:50:46 +0800

    bullet-tracer-rework: Initial Commit
    - Added a script that draws bullet tracers.
    - Added a new physics configuration to be used by project 45 projectiles
    - changed parameters of some standard bullet projectiles to use the new bullet tracer script.
    - TODO: test me in networked conditions

--- projectiles/scripts/project45-killprojectileonstick.lua
+++ projectiles/scripts/project45-killprojectileonstick.lua
@@ -7,7 +7,7 @@ function update(dt)
   if baseUpdate then baseUpdate(dt) end
   
   if projectile.collision() or mcontroller.isCollisionStuck() or mcontroller.isColliding() then
-	projectile.die()
+	  projectile.die()
   end
 end
 
@@ -18,4 +18,3 @@ function hit(entityId)
     projectile.die()
   end
 end
-
