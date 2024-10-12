f4cb0731b4d85734b66ce054c8edbfd3401b744d
Aikiro42 <hiroku042@gmail.com>
2024-10-13 03:32:46 +0800

    fix: improve project45bleed
    - project45bleed now deals 0.5% of maximum health as true damage, which increases based on how many times the effect is reapplied.
    - On reapplication, project45bleed increases the DoT damage by 100% and extends the effect's duration by 'stackLifetime' while refreshing the stack timer; e.g. Reapplying this effect 3 times increases the damage to 1.5% and extends it by approx. 3 * 'stackLifetime'.
      - There must be a short grace period between reapplications.
    - After 'stackLifetime' amount of time, the damage decreases by 100%.
      - e.g. From the previous example, after 'stackLifetime' seconds, the effect will then deal 1% damage.
      - The damage does not go below 100% damage, e.g. After 'stackLifetime' seconds from initial application (without reapplication), the effect will still deal 0.5% damage.
    - The effect can end before all its stacks have been used up.

--- stats/effects/project45bleed/project45bleed.lua
+++ stats/effects/project45bleed/project45bleed.lua
@@ -2,24 +2,53 @@ require "/scripts/util.lua"
 require "/scripts/status.lua"
 
 function init()
-  self.bleedTimer = 1
-  self.healthDamagePercentage = 0.03
+  
+  self.healthPercent = config.getParameter("healthDamagePercent") or 0.005
+  
+  self.bleedTimer = config.getParameter("bleedTimer") or 1
+  self.stacks = 1
+  self.stackLifetime = config.getParameter("stackLifetime") or 3
+  self.stackTimer = self.stackLifetime
+
+  self.prevDuration = effect.duration()
+  
   animator.playSound("wound")
 end
 
 function update(dt)
-  self.bleedTimer = self.bleedTimer - dt
-  if self.bleedTimer <= 0 then
-    self.bleedTimer = 1
-    status.applySelfDamageRequest({
-      damageType = "IgnoresDef",
-      damage = status.resourceMax("health") * self.healthDamagePercentage,
-      damageSourceKind = "default",
-      sourceEntityId = entity.id()
-    })
-    animator.playSound("bleed")
-    animator.burstParticleEmitter("bleed")
+
+  if self.prevDuration < effect.duration() then  -- reapplied
+    self.stacks = self.stacks + 1
+    self.stackTimer = self.stackLifetime
+    effect.modifyDuration(1)
   end
+
+  self.bleedTimer = self.bleedTimer - dt
+  self.stackTimer = self.stackTimer - dt
+  if self.bleedTimer <= 0 then
+    dealDamage(self.healthPercent, self.stacks)
+    if self.stackTimer <= 0 then
+      self.stacks = math.max(1, self.stacks - 1)
+      self.stackTimer = self.stackLifetime
+    end
+    self.bleedTimer = 1
+  end
+
+  self.prevDuration = effect.duration()
+
+
+end
+
+function dealDamage(healthPercent, stacks)
+  stacks = stacks or 1
+  status.applySelfDamageRequest({
+    damageType = "IgnoresDef",
+    damage = status.resourceMax("health") * healthPercent * stacks,
+    damageSourceKind = "default",
+    sourceEntityId = entity.id()
+  })
+  animator.playSound("bleed")
+  animator.burstParticleEmitter("bleed")
 end
 
 function uninit()
