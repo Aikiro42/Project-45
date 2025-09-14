03ac103ef52bbcd87341a6004e8c153844330dce
Aikiro42 <hiroku042@gmail.com>
2025-09-14 17:01:42 +0800

    fix: change reimburser sell factors
    - add system that allows certain items, tags or categories to have an increased sell factor
      - sell factor is multiplicative and considers the item's itemName, category and all its tags.

--- docs/branch-changelogs/bugfix.misc.md
+++ docs/branch-changelogs/bugfix.misc.md
@@ -4,3 +4,8 @@
 - Halved price of the Supply Transponder (Gacha puller) and both kinds of Cipher Keys (Gacha tickets).
 - Added Cipher Keys to the drop tables
 - Removed stray crafting recipe from the Gunsmith Addon
+
+- Decreased sell factor of the Reimburser from 1x to 0.8x.
+- Added increased selling prices for particular items sold via the Reimburser:
+  - reagents
+  - Unique Project 45 Items
--- interface/scripted/project45-gunseller/project45-gunseller.lua
+++ interface/scripted/project45-gunseller/project45-gunseller.lua
@@ -1,6 +1,31 @@
+function print(...)
+  return sb.logInfo(...)
+end
+
+function printJson(jsonValue)
+  return sb.logInfo(sb.printJson(jsonValue, 1))
+end
+
 function init()
-  local sellFactor = config.getParameter("sellFactor")
+  require "/scripts/set.lua"
+  
+  self.sellFactor = config.getParameter("sellFactor", 1)
   self.cachedItems = {}
+  self.appreciateItems = {}
+  self.appreciateTags = {}
+  self.appreciateCategories = {}
+  local appreciates = config.getParameter("appreciate", {nil})
+  for _, appreciate in ipairs(appreciates) do
+    if appreciate.item then
+      self.appreciateItems[appreciate.item] = appreciate.factor
+    end
+    if appreciate.tag then
+      self.appreciateTags[appreciate.tag] = appreciate.factor
+    end
+    if appreciate.category then
+      self.appreciateCategories[appreciate.category] = appreciate.factor
+    end
+  end
   --[[
   local acceptItems = config.getParameter("acceptItems")
   self.itemValues = {}
@@ -30,11 +55,28 @@ function valueOfContents()
   local value = 0
   local allItems = widget.itemGridItems("itemGrid")
   for _, item in pairs(allItems) do
+
+    -- obtain item config
     if not self.cachedItems[item.name] then
       self.cachedItems[item.name] = root.itemConfig(item.name)
     end
-    local itemCost = item.parameters.price or self.cachedItems[item.name].config.price or 0
-    value = value + itemCost * item.count
+    local itemConfig = self.cachedItems[item.name].config
+    local itemCategory = item.parameters.category or itemConfig.category or "unknown"
+    print(itemCategory)
+
+    -- get item cost
+    local itemCost = item.parameters.price or itemConfig.price or 0
+    
+    -- calculate item, category and tag factor
+    local itemFactor = self.appreciateItems[item.name] or 1
+    itemFactor = itemFactor * (self.appreciateCategories[itemCategory] or 1)
+    local itemTags = item.parameters.itemTags or itemConfig.itemTags or {nil}
+    for _, tag in ipairs(itemTags) do
+      itemFactor = itemFactor * (self.appreciateTags[tag] or 1)
+    end
+
+    -- add calculated itemvalue to item cost
+    value = value + itemCost * item.count * self.sellFactor * itemFactor
   end
   return value
 end
