f6b3991e4d594a334952abd9399c65f980f33868
Aikiro42 <hiroku042@gmail.com>
2025-09-21 01:32:02 +0800

    fix: unify all worldify functions

--- items/active/weapons/ranged/abilities/project45gunscope/project45gunscopeanimation.lua
+++ items/active/weapons/ranged/abilities/project45gunscope/project45gunscopeanimation.lua
@@ -1,6 +1,7 @@
 require "/scripts/vec2.lua"
 require "/scripts/util.lua"
 require "/scripts/poly.lua"
+require "/scripts/project45/project45util.lua"
 
 gunScope_oldUpdate = update or function() end
 
@@ -132,36 +133,7 @@ function renderLaser()
 -- that allows `localAnimator.addDrawable()`
 -- to render it correctly
 function worldify(pos1, pos2)
-    
-    local playerPos = activeItemAnimation.ownerPosition()
-    local worldLength = world.size()[1]
-    local fucky = {false, true, true, false}
-    
-    -- L is the x-size of the world
-    -- |0|--[1]--|--[2]--|L/2|--[3]--|--[4]--|L|
-    -- there is fucky behavior in quadrants 2 and 3
-    local pos1Quadrant = math.ceil(4*pos1[1]/worldLength)
-    local playerPosQuadrant = math.ceil(4*playerPos[1]/worldLength)
+  
+  return project45util.worldify(pos1, pos2)
 
-    local ducky = fucky[pos1Quadrant] and fucky[playerPosQuadrant]
-    local distance = world.distance(pos2, pos1)
-
-    local sameWorldSide = (pos1Quadrant > 2) == (playerPosQuadrant > 2)
-        
-    if ducky then
-        if (
-            (sameWorldSide and (pos1Quadrant > 2))
-            or (pos1Quadrant < playerPosQuadrant)
-        ) then
-            pos1[1] =  pos1[1] - worldLength
-        end
-    else
-        if pos1[1] > worldLength/2 then
-            pos1[1] = pos1[1] - worldLength
-        end
-    end
-
-    pos2 = vec2.add(pos1, distance)
-
-    return {pos1, pos2}
 end
