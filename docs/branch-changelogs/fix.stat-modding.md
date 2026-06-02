b2382ac7794d5cd8400bda08b4ee5a20e8c5999c
Aikiro42 <hiroku042@gmail.com>
2026-06-02 16:08:33 +0800

    fix: fix ammo mods overwriting parameters with config

--- docs/branch-changelogs/fix.stat-modding.md
+++ docs/branch-changelogs/fix.stat-modding.md
@@ -1,6 +1,7 @@
 # Fixes
 - Fixed how bad negative multiplicative stat bonuses are calculated. That is, modding a stat in a way that its denominator ("d") bonus end up in the negatives used to divide the stat, potentially upgrading the actual parameter instead of downgrading; this behavior is fixed.
 - Fixed issues with modding reload time.
+- Fixed issues with applying ammo mods before other mods, especially those with checks.
 
 # Changes (Modders)
 - Changed how stats under the "ergonomics" group are treated by the modding system.
--- scripts/augments/project45-gunmod/ammomod.lua
+++ scripts/augments/project45-gunmod/ammomod.lua
@@ -16,8 +16,8 @@ function apply(output, augment)
     -- assume conversion is done from apply.lua
 
     -- prepare to alter stats and the primary ability in general
-    local oldPrimaryAbility = output.config.primaryAbility or {}
-    local newPrimaryAbility = output:instanceValue("primaryAbility", {})
+    local oldPrimaryAbility = output:instanceValue("primaryAbility", {})
+    local newPrimaryAbility = input.parameters.primaryAbility or {}
 
     -- alter general projectile settings
     --[[
@@ -30,8 +30,7 @@ function apply(output, augment)
     end
     --]]
 
-    -- alter specific projectile settings
-    -- newPrimaryAbility.projectileKind = augment.projectileKind or oldPrimaryAbility.projectileKind -- no need; conversion already done
+    -- alter specific projectile settings in the primaryAbility
     local specificSettingsList = {"projectileType", "summonedProjectileType", "projectileParameters",
                                   "hitscanParameters", "beamParameters", "summonedProjectileParameters",
                                   "hideMuzzleFlash", "hideMuzzleSmoke"}
@@ -82,7 +81,6 @@ function apply(output, augment)
     end
 
     -- merge changes
-    -- FIXME: try installing an ammo mod then the automatic receiver.
     output:setInstanceValue("primaryAbility", sb.jsonMerge(oldPrimaryAbility, newPrimaryAbility))
 
     -- elemental type
