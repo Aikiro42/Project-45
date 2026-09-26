482534a3636143add4b5b96e9aaab2342914ee0f
Aikiro42 <hiroku042@gmail.com>
2026-09-26 11:14:31 +0800

    fix: add aim commitment to zoltraak
    - update spell circle sprite to have aim indicator
    - adjusted values to make commitment behavior more obvious
    - add prep code to use legacy hitscan method to damage enemies, including ghosts

--- projectiles/explosions/project45_zoltraakexplosion/project45zoltraak.lua
+++ projectiles/explosions/project45_zoltraakexplosion/project45zoltraak.lua
@@ -10,48 +10,60 @@ local oldUninit = uninit or function() end
 
 function init()
   oldInit()
-  self.targetRotation = mcontroller.rotation()
-  self.angleIncrement = config.getParameter("angularVelocity") or 0
+  self.currentRotation = mcontroller.rotation()
+  self.targetRotation = self.currentRotation
+  self.commitTime = config.getParameter("commitTime", 0.5)
 end
 
 function update(dt)
   oldUpdate(dt)
 
-  mcontroller.setRotation(mcontroller.rotation() + util.toRadians((self.angleIncrement * dt)))
-  self.angleIncrement = self.angleIncrement - (config.getParameter("angularVelocityDecay") or 0) * dt
+  self.currentRotation = self.currentRotation + ((self.targetRotation - self.currentRotation) * 0.2)
+  mcontroller.setRotation(self.currentRotation)
   
-  if self.targetPosition then
+  if self.targetPosition and self.commitTime > 0 then
     local toTarget = world.distance(self.targetPosition, mcontroller.position())
     self.targetRotation = vec2.angle(toTarget)
+    self.commitTime = self.commitTime - dt
   end
 
   if projectile.timeToLive() <= 0 then
     mcontroller.setRotation(self.targetRotation)
-    local pos = mcontroller.position()
-    local posIncrement = vec2.rotate({1.5, 0}, self.targetRotation)
-    for _=1, 50 do
-      local nextPos = vec2.add(pos, posIncrement)
-      local beamEnd = world.pointCollision(nextPos)
-      if beamEnd then
-        world.spawnProjectile(
-          "project45-stdexplosionshockwave",
-          pos,
-          projectile.sourceEntity(),
-          posIncrement,
-          false
-        )
-        break
-      end
+    hitscan()
+  end
+end
 
+function hitscan(vector, distance)
+  
+  local pos = mcontroller.position()
+  --[[
+  local targets = world.entityLineQuery(pos, vec2.add(pos, vec2.mul(vector, distance)), {
+    order = "nearest",
+    withoutEntityId=projectile.sourceEntity()
+  })
+  --]]
+  local posIncrement = vec2.rotate({1.5, 0}, self.targetRotation)
+  for _=1, 50 do
+    local nextPos = vec2.add(pos, posIncrement)
+    local beamEnd = world.pointCollision(nextPos)
+    if beamEnd then
       world.spawnProjectile(
-        "project45_zoltraaksegment",
+        "project45-stdexplosionshockwave",
         pos,
         projectile.sourceEntity(),
         posIncrement,
         false
       )
-      pos = nextPos
+      break
     end
+    world.spawnProjectile(
+      "project45_zoltraaksegment",
+      pos,
+      projectile.sourceEntity(),
+      posIncrement,
+      false
+    )
+    pos = nextPos
   end
 end
 
@@ -59,6 +71,7 @@ end
 function setTargetPosition(position)
   self.targetPosition = position
 end
+
 function uninit()
   oldUninit()
   mcontroller.setRotation(self.targetRotation)
